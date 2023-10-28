d870d8b5bc2c734b242e766ee519bb70e3ed10f7
CryptoJoe33 <117700080+CryptoJoe33@users.noreply.github.com>
2023-10-28 12:02:40 -0600

    Updated model field names

--- PayGate2023/payApp/models.py
+++ PayGate2023/payApp/models.py
@@ -3,18 +3,18 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    firstName = models.CharField(max_length=50)
-    lastName = models.CharField(max_length=50) 
-    email = models.CharField(max_length=50)
+    Username = models.CharField(max_length=50)
+    FirstName = models.CharField(max_length=50)
+    LastName = models.CharField(max_length=50) 
+    Email = models.CharField(max_length=50)
 
 class Transaction(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    amount = models.IntegerField()
-    cryptocurrency - models.charField(max_length=50)
+    User = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    Amoutn = models.IntegerField()
+    Cryptocurrency - models.charField(max_length=50)
 
 class Friends(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    email = models.CharField(max_length=50)
-    firstName = models.CharField(max_length=50)
-    lastName = models.CharField(max_length=50)
-
+    User = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    Email = models.CharField(max_length=50)
+    FirstName = models.CharField(max_length=50)
+    LastName = models.CharField(max_length=50)
