b08785f791b787e62d8ff81f8b5b856895061ff2
Y. Jace Liu <yang.jace.liu@linux.com>
2023-10-28 14:53:39 -0600

    Update component
    Signed-off-by: Y. Jace Liu <yang.jace.liu@linux.com>

--- paygate-ui/src/app/components/login-entry.js
+++ paygate-ui/src/app/components/login-entry.js
@@ -1,10 +1,12 @@
+import Image from "next/image";
+
 export const LoginEntry = (props) => {
     const {logo, name} = props;
 
     return (
         <div className="tw-p-6 tw-max-w-sm tw-mx-auto tw-bg-white tw-rounded-xl tw-flex tw-items-center tw-space-x-4">
             <div className="tw-shrink-0">
-                <img className="tw-h-12 tw-w-12" src={logo} alt="Logo" />
+                <Image className="tw-h-12 tw-w-12" src={logo} alt="Logo" />
             </div>
             <div>
                 <p className="tw-text-slate-800 tw-inline tw-align-middle">{name}</p>
--- paygate-ui/src/app/page.js
+++ paygate-ui/src/app/page.js
@@ -3,11 +3,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect } from "react";
 import { LoginEntry } from "./components/login-entry";
+import metamaskLogo from "../../public/metamask-logo.svg";
 
 export default function Home() {
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
 
-  return <LoginEntry logo="/metamask-logo.svg" name="MetaMask" />
+  return <LoginEntry logo={metamaskLogo} name="MetaMask" />
 }
