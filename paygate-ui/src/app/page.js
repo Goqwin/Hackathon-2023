8e20e4084f0741ac2aa940cff8bafc7e3854a584
Y. Jace Liu <yang.jace.liu@linux.com>
2023-10-28 14:55:33 -0600

    Remove invoker from main page
    Signed-off-by: Y. Jace Liu <yang.jace.liu@linux.com>

--- paygate-ui/src/app/page.js
+++ paygate-ui/src/app/page.js
@@ -2,13 +2,10 @@
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect } from "react";
-import { LoginEntry } from "./components/login-entry";
-import metamaskLogo from "../../public/metamask-logo.svg";
 
 export default function Home() {
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
-
-  return <LoginEntry logo={metamaskLogo} name="MetaMask" />
+  return <h1>Hello World</h1>;
 }
