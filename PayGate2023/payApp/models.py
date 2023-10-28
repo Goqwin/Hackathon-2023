a4ab8883635f1898f128b1d979adebad66f32c82
CryptoJoe33 <117700080+CryptoJoe33@users.noreply.github.com>
2023-10-28 11:50:03 -0600

    Models created

--- PayGate2023/payApp/models.py
+++ PayGate2023/payApp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50) 
+    email = models.CharField(max_length=50)
+
+class Transaction(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    cryptocurrency - models.charField(max_length=50)
+
+class Friends(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    email = models.CharField(max_length=50)
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+
